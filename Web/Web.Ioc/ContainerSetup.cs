4f86b162fd245ca798422043667eca6dc4e8dff1
xxxmoney <xxxmoney111@gmail.com>
2023-02-19 14:08:02 +0100

    Added logger factory, disabled nullable, other.

---
+++ Web/Web.Ioc/ContainerSetup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Ioc
+{
+    public interface IContainerSetup
+    {
+        IServiceCollection Configure(IServiceCollection serviceCollection);
+    }
+
+    public class ContainerSetup : IContainerSetup
+    {
+        public IServiceCollection Configure(IServiceCollection serviceCollection)
+        {
+            // Repositories
+
+            // Operations
+
+            // Services
+            
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ Web/Web.Logger/LoggerFactory.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Logger
+{
+    /// <summary>
+    /// Factory for database logger.
+    /// </summary>
+    public interface ILoggerFactory
+    {
+        /// <summary>
+        /// Creates a new instance of database logger.
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        ILogger CreateLogger(Configuration configuration);
+    }
+
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger(Configuration configuration)
+        {
+            return new LoggerConfiguration()
+                .WriteTo.MSSqlServer(
+                    connectionString: configuration.ConnectionString,
+                    columnOptions: configuration.ColumnOptions,
+                    sinkOptions: new MSSqlServerSinkOptions
+                    {
+                        AutoCreateSqlTable = configuration.AutoCreateTable,
+                        TableName = configuration.TableName,
+                        SchemaName = configuration.SchemaName
+                    })
+                .CreateLogger();
+
+        }
+    }
+}
